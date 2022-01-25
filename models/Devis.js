c9fc24831de3e013a976207ab6c4423a01ef044f
HichemHa <hichemhanafi012@gmail.com>
2022-01-25 14:49:10 +0100

    25-01

--- models/Devis.js
+++ models/Devis.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 var nextRunDate = new Date();
 nextRunDate.setMonth(nextRunDate.getMonth() + 3);
 
 const deviSchema = mongoose.Schema({
-    numerodevis:Number,
-    datedevis:{type:Date,default: Date.now()},
-    datevalidite:{type:Date,default: nextRunDate},
-    client:{ type: mongoose.Types.ObjectId, ref: "Client" },
-    listproduit:[
-        {
-            produit:{ type: mongoose.Types.ObjectId, ref: "Product" },
-            quantit:Number,
-            prixtot:{type:Number,default:0},
-        }
-    ]
-})
+  numerodevis: Number,
+  datedevis: { type: Date, default: Date.now() },
+  datevalidite: { type: Date, default: nextRunDate },
+  client: { type: mongoose.Types.ObjectId, ref: "Client" },
+  listproduit: [
+    {
+      prod: {},
+      qnt:Number
+    },
+  ],
 
-module.exports = Devis = new mongoose.model("Devis",deviSchema);
+
+  prixtot: { type: Number, default: 0 },
+  status:{type:String,default:"in progress"}
+});
+
+module.exports = Devis = new mongoose.model("Devis", deviSchema);
--- routes/devis.js
+++ routes/devis.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Devis = require("../models/Devis");
+const Product = require("../models/Product")
 const router = express.Router();
 
 router.get("/getalldevis", async (req, res) => {
@@ -20,6 +21,34 @@ router.post("/adddevis", async (req, res) => {
     await res.status(201).json({ msg: `Devis added successfully` });
   } catch (error) {
     console.error("Devis register failed", error);
+    res.status(401).json({ msg: `Devis register Failed` });
+  }
+});
+
+router.get("/getlistproduitindevis", async(req,res)=>{
+  try {
+    const xxxx = await Devis.find({},{listproduit:1});
+    const pp = await Product.find();
+    const xxx = xxxx.map(el=>el.listproduit).map(e=>e.map(x =>x.produit));
+    const zz =[];
+    
+    
+        await res.status(200).json(xxx);
+
+  } catch (error) {
+    console.error("Devis get failed", error);
+    res.status(401).json({ msg: `Devis get Failed` });
+    
+  }
+})
+
+router.delete("/delete/:id", async (req, res) => {
+  const idd = req.params.id;
+  try {
+    await Devis.findOneAndDelete(idd);
+    res.status(201).json({ msg: `Devis deleted successfully` });
+  } catch (error) {
+    console.error("Devis deleted failed", error);
     res.status(401).json({ msg: `Devis register Failed` });
   }
 });
